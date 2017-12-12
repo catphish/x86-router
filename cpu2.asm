3ba08d22bba4e0e59abf21df0fcfe4c024b00906
Charlie Smurthwaite <charlie@atechmedia.com>
2017-12-12 20:54:31 +0000

    finish ap asm entry

--- cpu2.asm
+++ cpu2.asm
@@ -6,37 +6,52 @@ extern ap_c_entry
 
 ap_asm_start:
   jmp past_header
-
   align 8
   ap_asm_length: dd ap_asm_end - ap_asm_start
-  dd 0
+  dd 0         ; Padding
 
-  dw 0
+  dw 0         ; Padding
 gdt_pointer:
-  dw 23 ; limit (Size of GDT)
-  dd gdt_data                  ; base of GDT
+  dw 23        ; limit (Size of GDT)
+  dd gdt_data  ; base of GDT
 
+pm_entry_pointer:
+  dd 0         ; Far jump location
+  dw 0x8       ; Far jump segment
+  dw 0         ; Padding
 past_header:
+  ; Disable interrupts
   cli
-  ; enable protected mode in the cr0 register
+  ; Initialize stack. TODO: one per CPU
+  mov esp, stack_top
+  ; Load the GDT
+  lgdt [cs:gdt_pointer - ap_asm_start]
+  ; Calculate location for far jump
+  xor eax,eax
+  mov ax,cs
+  shl eax,4
+  add eax, pm_entry - ap_asm_start
+  ; Place calculated location into memory
+  mov [cs:pm_entry_pointer - ap_asm_start], eax
+  ; Enable protected mode in the cr0 register
   mov eax, cr0
   or eax, 0x1
   mov cr0, eax
-
-  mov esp, stack_top
-
-  lgdt [cs:gdt_pointer - ap_asm_start]
+  ; Far jump to calculated location of pm_entry
+  o32 jmp far [cs:pm_entry_pointer - ap_asm_start]
+[bits 32]
+pm_entry:
+  ; Load segment registers
   mov ax, 0x10
   mov ds, ax
   mov ss, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
-
-  jmp 0x8:dword ap_c_entry
+  ; Far jump again to C entry point
+  jmp 0x8:ap_c_entry
 ap_asm_end:
 
-[bits 32]
 
 section .bss
 align 4096
